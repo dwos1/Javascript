eb61595f5fe0bfbbd9cab0b5d4e1a18e19b1e0c7
David Willians <davidwillian.neto@gmail.com>
2026-07-29 15:45:06 -0300

    feat: cria cadastro simples com prompt-sync

--- variaveis.js
+++ variaveis.js
@@ -1,18 +1,16 @@
-
-let media = 0;
 const prompt = require('prompt-sync')();
 
-const nome = (prompt("Digite o nome do aluno: "));
-const nota1 = Number(prompt("Digite a nota 1: "));
-const nota2 = Number(prompt("Digite nota 2: "));
+const nome  = prompt("Digite seu nome: ");
+const idade = Number(prompt("Digite sua idade: "))
+const salario = Number(prompt("Digite seu salário: "));
 
-media = (nota1+nota2)/2;
-
-console.log("O aluno: ", nome);
-console.log("A média é: ", media);
-
-if(media>=7){
-    console.log("O aluno foi aprovado");
-}else if(media>=5){
-    console.log("O aluno ficou de recuperação");
-}else{console.log("O aluno foi reprovado");}
+if(nome === ""){
+    console.log("Nome inválido");
+}else if(idade<0 || idade>120){
+    console.log("A idade é inválida");
+}else if(salario<0){
+    console.log("Salário Inválido");
+}else{console.log("\nO nome é: ", nome);
+    console.log("\nA idade é: ", idade);
+    console.log("\nO salário é: ", salario);   
+}

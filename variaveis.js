56532e5b9ff497908658e380c7a4ab8f0c94269b
David Willians <davidwillian.neto@gmail.com>
2026-07-29 14:09:44 -0300

    Média de alunos

--- variaveis.js
+++ variaveis.js
@@ -1,14 +1,18 @@
-const saldo = 300;
-const precoProduto = 300;
-const contaAtiva = true;
-const produtoDisponivel = true;
-if(saldo<0 || saldo <=0){
-    console.log("Dados Inválidos")
-}
-else if(saldo<precoProduto){
-    console.log("Não tem dinheiro necessário");
-}else if(contaAtiva === false){
-    console.log("A conta está desativa");
-}else if(produtoDisponivel  === false){
-    console.log("O produto não está disponível");
-}else{console.log("O produto foi comprado");}
+
+let media = 0;
+const prompt = require('prompt-sync')();
+
+const nome = (prompt("Digite o nome do aluno: "));
+const nota1 = Number(prompt("Digite a nota 1: "));
+const nota2 = Number(prompt("Digite nota 2: "));
+
+media = (nota1+nota2)/2;
+
+console.log("O aluno: ", nome);
+console.log("A média é: ", media);
+
+if(media>=7){
+    console.log("O aluno foi aprovado");
+}else if(media>=5){
+    console.log("O aluno ficou de recuperação");
+}else{console.log("O aluno foi reprovado");}

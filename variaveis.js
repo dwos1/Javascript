1e0f0bbe2390fd59440dd69d3fef7535c2a2f7a6
David Willians <davidwillian.neto@gmail.com>
2026-07-30 12:06:23 -0300

    study: pratica validação de dados com trim e Number.isNaN

--- variaveis.js
+++ variaveis.js
@@ -1,16 +1,19 @@
 const prompt = require('prompt-sync')();
 
-const nome  = prompt("Digite seu nome: ");
-const idade = Number(prompt("Digite sua idade: "))
-const salario = Number(prompt("Digite seu salário: "));
+const nome = prompt("Digite o nome: ");
+const idade = Number(prompt("Digite a idade: "));
+const salario = Number(prompt("Digite o salário: "));
 
-if(nome === ""){
-    console.log("Nome inválido");
+
+if(nome.trim() === ""){
+    console.log("nome inválido")
+}else if(Number.isNaN(idade)){
+    console.log("Idade inválido")
 }else if(idade<0 || idade>120){
-    console.log("A idade é inválida");
-}else if(salario<0){
-    console.log("Salário Inválido");
-}else{console.log("\nO nome é: ", nome);
-    console.log("\nA idade é: ", idade);
-    console.log("\nO salário é: ", salario);   
+    console.log("Idade inválido")
+}else if(Number.isNaN(salario) || salario < 0){
+    console.log("Salário inválido")
+}else{console.log("O nome é: ", nome)
+        console.log("A idade é: ", idade)
+        console.log("O salário é: ", salario)
 }

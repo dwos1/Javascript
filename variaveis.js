e4b7680ff48e61a0b956a4c244c900bf6ff372f9
David Willians <davidwillian.neto@gmail.com>
2026-07-30 18:05:07 -0300

    estudo

--- variaveis.js
+++ variaveis.js
@@ -1,19 +1,24 @@
 const prompt = require('prompt-sync')();
-
-const nome = prompt("Digite o nome: ");
-const idade = Number(prompt("Digite a idade: "));
-const salario = Number(prompt("Digite o salário: "));
-
-
-if(nome.trim() === ""){
-    console.log("nome inválido")
-}else if(Number.isNaN(idade)){
-    console.log("Idade inválido")
-}else if(idade<0 || idade>120){
-    console.log("Idade inválido")
-}else if(Number.isNaN(salario) || salario < 0){
-    console.log("Salário inválido")
-}else{console.log("O nome é: ", nome)
-        console.log("A idade é: ", idade)
-        console.log("O salário é: ", salario)
+const dinheiroDisponivel = 200;
+let troco = 0; 
+const nomeProduto = prompt("Qual é nome do produto: ");
+const precoProduto = Number (prompt("Qual é preço do produto: R$ "));
+const quantidade = Number(prompt("Quantidade: "));
+const total = precoProduto*quantidade;
+if(nomeProduto.trim ()=== ""){
+    console.log("\nNome Inválido");
+}else if(Number.isNaN(precoProduto)){
+    console.log("Número Produto Inválido");
+}else if(Number.isNaN(quantidade)){
+    console.log("Quantidade Indisponível");
+}
+else if(precoProduto>dinheiroDisponivel){
+    console.log("Dinheiro Indisponóvel");
+}else if(total>dinheiroDisponivel){
+    console.log("Dinheiro Indisponivel");
+}else{console.log("\nO nome do Produto: ", nomeProduto);
+    console.log("\nO nome do Preço do Produto: ", precoProduto);
+    troco =  dinheiroDisponivel - (total) 
+    console.log("\nO troco vai ser: ", troco);
+    
 }

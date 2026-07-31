bdcee83d48be5c606829d33c100ad01070c768c2
David Willians <davidwillian.neto@gmail.com>
2026-07-31 17:49:01 -0300

    Estudo do While

--- variaveis.js
+++ variaveis.js
@@ -1,24 +1,13 @@
-const prompt = require('prompt-sync')();
-const dinheiroDisponivel = 200;
-let troco = 0; 
-const nomeProduto = prompt("Qual é nome do produto: ");
-const precoProduto = Number (prompt("Qual é preço do produto: R$ "));
-const quantidade = Number(prompt("Quantidade: "));
-const total = precoProduto*quantidade;
-if(nomeProduto.trim ()=== ""){
-    console.log("\nNome Inválido");
-}else if(Number.isNaN(precoProduto)){
-    console.log("Número Produto Inválido");
-}else if(Number.isNaN(quantidade)){
-    console.log("Quantidade Indisponível");
-}
-else if(precoProduto>dinheiroDisponivel){
-    console.log("Dinheiro Indisponóvel");
-}else if(total>dinheiroDisponivel){
-    console.log("Dinheiro Indisponivel");
-}else{console.log("\nO nome do Produto: ", nomeProduto);
-    console.log("\nO nome do Preço do Produto: ", precoProduto);
-    troco =  dinheiroDisponivel - (total) 
-    console.log("\nO troco vai ser: ", troco);
-    
+const prompt = require("prompt-sync")();
+
+const numero = Number(prompt("Digite o número: "));
+
+let contador = 0;
+let resultado = 0;
+
+while (contador < 10) {
+    contador++;
+    resultado = numero * contador;
+
+    console.log(numero, "x", contador, "=", resultado);
 }

c92b287f8a63814f302ee4394cbab7ffaacba113
dwos1 <davidwillian.neto@gmail.com>
2026-08-08 18:09:36 -0300

    functions média com if and else

--- variaveis.js
+++ variaveis.js
@@ -1,14 +1,21 @@
-function verificarNota(nota){
-  if(nota>=7){
+const prompt = require("prompt-sync")();
+let nota1 = Number(prompt("Digite a nota 1: "))
+let nota2 = Number(prompt("Digite a nota 2: "))
+const media = 0;
+
+function verificarNota(nota1, nota2){
+
+    const media = (nota1 + nota2) / 2;
+  if(Number.isNaN(nota1) || Number.isNaN(nota2) || nota1>10 || nota2>10 ||  nota1 < 0 || nota2 < 0){
+    return("erro")
+  }else if(media>=7){
     return("Aluno passou")
-  }else if(nota>=5){
+  }else if(media>=5){
     return("Aluno de recuperação")
   }else{
     return("Aluno reprovou")
   }
 }
 
-const resultado = verificarNota(10);
 
-console.log(resultado)
-
+console.log(verificarNota(nota1,nota2));

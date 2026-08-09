17aaa9a6dbd7c2222db399d0014d8ffdc22ebff9
dwos1 <davidwillian.neto@gmail.com>
2026-08-09 16:11:25 -0300

    functions

--- variaveis.js
+++ variaveis.js
@@ -1,12 +1,10 @@
-const prompt = require('prompt-sync')();
-let numero = Number(prompt("Digite a primeira numero: "));
-     
-function tabuada(numero){
-  for(let contador = 1; contador<=10; contador++){
-     const resultado = numero * contador;
-      console.log(numero, "X", contador, "=", resultado);
-  }
-  
+const valor = 10;
+
+function calcular(valor) {
+    const resultado = valor + 5;
+    console.log(resultado);
 }
 
-tabuada(numero);
+calcular(20);
+
+console.log(valor);

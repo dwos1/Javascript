2bd96c512dcaabfa0e0a97b438715bac68174a8a
dwos1 <davidwillian.neto@gmail.com>
2026-08-08 21:09:57 -0300

    functions + for

--- variaveis.js
+++ variaveis.js
@@ -1,21 +1,12 @@
-const prompt = require("prompt-sync")();
-let nota1 = Number(prompt("Digite a nota 1: "))
-let nota2 = Number(prompt("Digite a nota 2: "))
-const media = 0;
-
-function verificarNota(nota1, nota2){
-
-    const media = (nota1 + nota2) / 2;
-  if(Number.isNaN(nota1) || Number.isNaN(nota2) || nota1>10 || nota2>10 ||  nota1 < 0 || nota2 < 0){
-    return("erro")
-  }else if(media>=7){
-    return("Aluno passou")
-  }else if(media>=5){
-    return("Aluno de recuperação")
-  }else{
-    return("Aluno reprovou")
+const prompt = require('prompt-sync')();
+let numero = Number(prompt("Digite a primeira numero: "));
+     
+function tabuada(numero){
+  for(let contador = 1; contador<=10; contador++){
+     const resultado = numero * contador;
+      console.log(numero, "X", contador, "=", resultado);
   }
+  
 }
 
-
-console.log(verificarNota(nota1,nota2));
+tabuada(numero);

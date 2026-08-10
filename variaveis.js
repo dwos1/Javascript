235ee914594dd472782310c25b18b96410a08250
David Willians <davidwillian.neto@gmail.com>
2026-08-10 17:08:44 -0300

    functions

--- variaveis.js
+++ variaveis.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const valor = 10;
 
 function calcular(valor) {
@@ -8,3 +9,20 @@ function calcular(valor) {
 calcular(20);
 
 console.log(valor);
+=======
+const numero1 = 3;
+const numero2 = 3;
+
+function multiplicacao(numero1,numero2){
+        return numero1 * numero2;
+
+}
+
+function mostrarMultiplicacao(numero1, numero2){
+
+        console.log(multiplicacao(numero1,numero2))
+}
+
+
+mostrarMultiplicacao(8,8);
+>>>>>>> 052fff4 (functions)

7d949337a8e417e4051f8d466632b1b4c6ded0e9
David Willians <davidwillian.neto@gmail.com>
2026-08-11 17:45:50 -0300

    estudo de funções

--- variaveis.js
+++ variaveis.js
@@ -1,15 +1,8 @@
-const numero1 = 3;
-const numero2 = 3;
-
-function multiplicacao(numero1,numero2){
-        return numero1 * numero2;
-
+function mediaEscola(nota1,nota2){
+        return (nota1+nota2) / 2;
+}
+function mostrarMedia(nota1,nota2){
+        console.log(mediaEscola(nota1,nota2));
 }
 
-function mostrarMultiplicacao(numero1, numero2){
-
-        console.log(multiplicacao(numero1,numero2))
-}
-
-
-mostrarMultiplicacao(8,8);
+mostrarMedia(9,5);

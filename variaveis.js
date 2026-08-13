8ff091d1f1fc6ba7c301f956eb8c280b95af8a99
David Willians <davidwillian.neto@gmail.com>
2026-08-13 17:27:43 -0300

    estudo de funções

--- variaveis.js
+++ variaveis.js
@@ -1,10 +1,14 @@
 function verificarIdade(idade){
-    if(Number.isNaN(idade) || !Number.isInteger(idade) || idade<0 || idade>120){
-        return "erro";
-    }else if(idade>=18){
-        return "maior de idade";
+    if( Number.isNaN(idade) || idade<0 || idade>120){
+        return "erro";} 
+    else if(idade>=12 && idade<=17){
+        return "adolescente";
+    }else if(idade>=18 && idade<=59 ){
+        return "adulto";
+    }else if(idade>=60){
+        return "idoso";
     }else{
-        return "menor de idade";
+        return "criança";
     }
 }
 
@@ -12,4 +16,4 @@ function mostrarIdade(idade){
     console.log(verificarIdade(idade));
 }
 
-mostrarIdade(129);
+mostrarIdade(120);

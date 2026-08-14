bc727ec72115754c941dfc44ac617a0db94c9089
David Willians <davidwillian.neto@gmail.com>
2026-08-14 16:06:40 -0300

    estudo de funções

--- variaveis.js
+++ variaveis.js
@@ -1,13 +1,19 @@
-function investir(compra){
-    if(compra>=10 || compra<=0){
-            return false;
+function validarMedia(nota1, nota2){
+    const media = (nota1+nota2) / 2
+    if(Number.isNaN(nota1) || Number.isNaN(nota2) || nota1<0 || nota2<0 || nota2>10 || nota1>10){
+        return "erro";
+    }else if(media<5){
+        return "reprovado";
+    }else if(media<7){
+        return "recuperação";
+    }else if(media<9){
+        return "aprovado";
     }else{
-        return true;
+        return "excelente";
     }
 }
 
-function investirBolsa(compra){
-    console.log(investir(compra));
+function mostrarMedia(nota1, nota2){
+    console.log(validarMedia(nota1,nota2));
 }
-
-investirBolsa(8);
+mostrarMedia(8,5)

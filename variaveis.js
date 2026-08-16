9a9bd485f6e6e334be1eb0b82bf3fbf20bb56bab
dwos1 <davidwillian.neto@gmail.com>
2026-08-15 21:16:02 -0300

    functions

--- variaveis.js
+++ variaveis.js
@@ -1,12 +1,20 @@
 function porta(senha){
+<<<<<<< HEAD
     if(senha === 1234){
         return true;
     }else{
        return false;
+=======
+    if(senha == 1234){
+        return true;
+    }else{
+        return false;
+>>>>>>> 80c08b7 (functions)
     }
 }
 
 function mostrarPorta(senha){
+<<<<<<< HEAD
     if (porta(senha)){
         return "porta aberta";
     }else{
@@ -16,3 +24,13 @@ function mostrarPorta(senha){
 
 console.log(mostrarPorta(1234))
 
+=======
+    if(porta = 1234){
+        console.log("Pode abrir");
+    }else{
+        console.log("Não pode abrir");
+    }
+}
+
+mostrarPorta(1234);
+>>>>>>> 80c08b7 (functions)

be3eacf752882b9acc8c970feb69e68f3448420a
David Willians <davidwillian.neto@gmail.com>
2026-08-17 16:17:36 -0300

    Estudo

--- variaveis.js
+++ variaveis.js
@@ -1,9 +1,10 @@
 function contarPares(numero){
     let par = 0;
     for(let contador = 2; contador<=numero; contador = contador + 2){
-        par = contador + par;
         
+        par = par + 1;
     }
+    
     console.log(par)
 }
 

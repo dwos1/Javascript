885d52f11f396d6f0eaefbfab1bd5ef7a11249b1
David Willians <davidwillian.neto@gmail.com>
2026-08-17 15:48:50 -0300

    Estudo

--- variaveis.js
+++ variaveis.js
@@ -1,9 +1,9 @@
-function somar(numero){
+function soma(numero){
     let soma = 0;
-    for(let contador = 1; contador<=numero; contador++){
-        soma = soma + contador;
-        console.log(soma)
-    }
+    for(let contador= 0; contador<=numero; contador = contador + 2){
+     soma = soma + contador;
 }
-
-somar(10);
+    console.log(soma);
+ }
+    
+soma(10);

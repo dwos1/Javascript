5c3864062330085c66186ca2a91863edc56403ba
David Willians <davidwillian.neto@gmail.com>
2026-08-20 10:07:37 -0300

    Estudo de quinta

--- variaveis.js
+++ variaveis.js
@@ -1,10 +1,10 @@
-function contarPares(numero){
+function regressivaPares(numero){
     let par = 0;
-    for(let contador = 2; contador<=numero; contador = contador + 2 ){
-        par = par + 1;
-
+    for(let contador = numero; contador>=1; contador = contador - 2){
+        par = contador + par;
+        
     }
     console.log(par)
 }
 
-contarPares(12);
+regressivaPares(10)

618bfb9d2d7537ec972727f0153efa9d32ef0962
David Willians <davidwillian.neto@gmail.com>
2026-08-21 17:35:13 -0300

    Estudo de quinta

--- variaveis.js
+++ variaveis.js
@@ -1,13 +1,14 @@
-function somaDePares(numero){
+function contadorDePares(numero){
     let contador = 2;
     let par = 0;
     while(contador<=numero){
-        par = contador + par;
+        par = 1 + par;
         contador = contador + 2;
+        
          
 }
         console.log(par)
     }
    
 
-somaDePares(10);
+contadorDePares(10);

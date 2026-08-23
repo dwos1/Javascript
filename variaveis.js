fb1f0bb8f497fe3dca19be2f696daf266904279a
dwos1 <davidwillian.neto@gmail.com>
2026-08-23 20:45:27 -0300

    chore: configura projeto Node.js

--- variaveis.js
+++ variaveis.js
@@ -1,23 +1,27 @@
 function nota(nota1, nota2){
-    if(nota1>=0 && nota1<=10 && nota2>=0 && nota2<=10){
-        
+    if(nota1 >= 0 && nota1 <= 10 && nota2 >= 0 && nota2 <= 10){
         return true;
     }else{
         return false;
     }
+}
 
+function calcularMedia(nota1, nota2){
+    return (nota1 + nota2) / 2;
+    
 }
 
 function validarNota(nota1, nota2){
-    let calcular = (nota1 + nota2) / 2;
-    if(nota(nota1,nota2) ){ 
-    
-    console.log(calcular)
-}else{
-    console.log("erro")
-}
-    console.log(nota(nota1,nota2));
+   
+
+    if(nota(nota1, nota2)){
+        console.log(calcularMedia(nota1, nota2));
+    }else{
+        console.log("erro");
+    }
 }
 
 validarNota(10,10);
+
+
 

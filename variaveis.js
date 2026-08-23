47ddb2c32bf875205df20da0b8f50b8260481fd8
dwos1 <davidwillian.neto@gmail.com>
2026-08-23 17:19:40 -0300

    chore: configura projeto Node.js

--- variaveis.js
+++ variaveis.js
@@ -1,15 +1,10 @@
-function somarImpares(numero){
-   let contador = 1;
-   let impar = 0;
+function multiplicarAte(numero){
+    let multiplicar = 1;
+    let contador = 1;
     while(contador<=numero){
+        multiplicar = multiplicar * contador;
+        contador = contador + 1;
             
-            impar = impar + contador;
-            contador = contador + 2;
-           console.log(impar)
-            
-    
-        }
-     
+    }
+    console.log(multiplicar)
 }
-
-somarImpares(9);

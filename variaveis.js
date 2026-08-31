df8221eb67729bf38a35b1359939a39a86230d52
David Willians <davidwillian.neto@gmail.com>
2026-08-31 17:38:45 -0300

    Estudo de sextaa

--- variaveis.js
+++ variaveis.js
@@ -1,12 +1,3 @@
-function nota(nota1){
-    if(nota1>=7){
-        return true;
-    }else{
-        return false;
-    }
-}
-function validarNota(nota1){
-    console.log(nota(nota1));
-}
+let nome = ["david", "willians" , "silva"];
 
-validarNota(8);
+console.log(nome[2])

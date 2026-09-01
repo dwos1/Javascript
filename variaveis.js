feccc8e4627cb3101143c9977d5a6321cf274815
David Willians <davidwillian.neto@gmail.com>
2026-09-01 18:10:02 -0300

    Estudo de sextaa

--- variaveis.js
+++ variaveis.js
@@ -1,6 +1,21 @@
-let nome = ["david", "willians" , "silva"];
+function alunoNota(numero1,numero2){
+    if(numero1>=0 && numero1<=10 && numero2>=0 && numero2<=10 ){
+        return true;
+    }else{
+        return false;
+    }
+}
 
+function mediaNota(numero1,numero2){
+    return (numero1 + numero2) / 2;
+}
 
-nome.pop();
+function notaTotal(numero1, numero2){
+    if(alunoNota(numero1,numero2)){
+        console.log(mediaNota(numero1,numero2));
+    }else{
+        console.log("erro");
+    }
+}
 
-console.log(nome)
+notaTotal(8,11);

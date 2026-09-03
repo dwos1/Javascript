23f1dba687de1826658d7172c76b3e1fc1fcff8a
David Willians <davidwillian.neto@gmail.com>
2026-09-03 11:30:29 -0300

    Estudo de sextaa

--- variaveis.js
+++ variaveis.js
@@ -1,21 +1,7 @@
-function alunoNota(numero1,numero2){
-    if(numero1>=0 && numero1<=10 && numero2>=0 && numero2<=10 ){
-        return true;
-    }else{
-        return false;
-    }
-}
+let pais = ["Dinamarca", "Holanda", "França"]
 
-function mediaNota(numero1,numero2){
-    return (numero1 + numero2) / 2;
-}
+pais.pop();
+pais.push("Venezuela")
+pais.unshift();
 
-function notaTotal(numero1, numero2){
-    if(alunoNota(numero1,numero2)){
-        console.log(mediaNota(numero1,numero2));
-    }else{
-        console.log("erro");
-    }
-}
-
-notaTotal(8,11);
+console.log(pais)

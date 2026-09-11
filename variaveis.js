18dd12b80d6f26f9b28a426a9137ac06430e5a07
David Willians <davidwillian.neto@gmail.com>
2026-09-11 16:33:47 -0300

    Estudo de sextaa

--- variaveis.js
+++ variaveis.js
@@ -1,21 +1,3 @@
-function validarNota(nota1,nota2){
-    if(nota1>=1 && nota1<=10 && nota2>=1 && nota2<=10){
-        return true;
-    }else{
-        return false;
-    }
-}
+let name = ["Paulo", "Cassio", "Lucas"]
 
-function calculoNota(nota1,nota2){
-    return (nota1 + nota2) / 2;
-}
-
-function mediaNota(nota1,nota2){
-    if(validarNota(nota1,nota2)){
-        console.log(calculoNota(nota1,nota2));
-    }else{
-        console.log("erro")
-    }
-}
-
-mediaNota(9,9);
+console.log(name)

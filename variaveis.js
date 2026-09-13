1e66002af0b70bebc88ddd44d3ed71a3e38f8980
dwos1 <davidwillian.neto@gmail.com>
2026-09-13 16:34:22 -0300

    curso

--- variaveis.js
+++ variaveis.js
@@ -1,3 +1,27 @@
+<<<<<<< HEAD
 let name = ["Paulo", "Cassio", "Lucas"]
 
 console.log(name)
+=======
+function contaBancaria(dinheiro){
+    if(dinheiro>=0){
+        return true;
+    }else{
+        return false
+    }
+}
+
+function validarBancario(dinheiro){
+    return dinheiro;
+}
+
+function quantidadeBanco(dinheiro){
+    if(contaBancaria(dinheiro)){
+        console.log(validarBancario(dinheiro))
+    }else{
+        console.log("Erro")
+    }
+}
+
+quantidadeBanco(-2000);
+>>>>>>> 665a3be (sabado)

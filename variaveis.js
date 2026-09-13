6e92accce1b3d0d21ab5dd53cbabcbf49e471f54
dwos1 <davidwillian.neto@gmail.com>
2026-09-13 16:28:03 -0300

    sabado

--- variaveis.js
+++ variaveis.js
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 let name = ["Paulo", "Cassio", "Lucas"]
 
 console.log(name)
@@ -6,11 +7,30 @@ console.log(name)
 function contaBancaria(dinheiro){
     if(dinheiro>=0){
         return true;
+=======
+function validarAcao(acao){
+        if(acao>=5){
+            return true;
+        }else{
+            return false;
+        }
+}
+
+
+function contagemAcao(acao){
+    return acao;
+}
+
+function mostrarAcao(acao){
+    if(validarAcao(acao)){
+        console.log(contagemAcao(acao));
+>>>>>>> cafdf79 (sabado)
     }else{
-        return false
+        console.log("erro")
     }
 }
 
+<<<<<<< HEAD
 function validarBancario(dinheiro){
     return dinheiro;
 }
@@ -25,3 +45,6 @@ function quantidadeBanco(dinheiro){
 
 quantidadeBanco(-2000);
 >>>>>>> 665a3be (sabado)
+=======
+mostrarAcao(30);
+>>>>>>> cafdf79 (sabado)

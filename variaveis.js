9d0cdc501dec49de99f228dfa9d381943038638a
dwos1 <davidwillian.neto@gmail.com>
2026-09-20 23:26:15 -0300

    curso

--- variaveis.js
+++ variaveis.js
@@ -1,5 +1,5 @@
-let name = ["David", "Cássio", "Lucas"];
+let numero = [1,2,3,4,5,6,7,8,9,10]
 
-name.push("Vanderson")
-
-console.log(name)
+for(let contador = 1; contador <= numero.length; contador++){
+    console.log(contador)
+}

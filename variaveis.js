ba06fb94dfd849e7c39f97af9f91182d447a6f26
dwos1 <davidwillian.neto@gmail.com>
2026-09-20 23:36:30 -0300

    curso

--- variaveis.js
+++ variaveis.js
@@ -1,5 +1,3 @@
-let numero = [1,2,3,4,5,6,7,8,9,10]
-
-for(let contador = 1; contador <= numero.length; contador++){
+for(let contador = 10; contador >= 2; contador = contador - 2){
     console.log(contador)
 }

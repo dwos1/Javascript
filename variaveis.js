bd564791f86e9d640c77aea6b466f6e73ea631f7
dwos1 <davidwillian.neto@gmail.com>
2026-09-20 21:49:17 -0300

    curso

--- variaveis.js
+++ variaveis.js
@@ -1,22 +1,5 @@
-<<<<<<< HEAD
-function validarNota(nota1,nota2){
-    if(nota1<=10 && nota1>=0 && nota2<=10 && nota2>=0){
-        return true;
-    }else{
-        return false
-    }
-}
+let name = ["David", "Cássio", "Lucas"];
 
-function contaNota(nota1,nota2){
-    return (nota1 + nota2) / 2
-}
+name.push("Vanderson")
 
-function totalNota(nota1,nota2){
-    if(validarNota(nota1,nota2)){
-        console.log(contaNota(nota1,nota2))
-    }else{
-        console.log("erro")
-    }
-}
-
-totalNota(9,9)
+console.log(name)

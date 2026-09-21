92a53c1280ce1b3c77d9b3633a710330e7dac057
dwos1 <davidwillian.neto@gmail.com>
2026-09-20 21:33:18 -0300

    curso

--- variaveis.js
+++ variaveis.js
@@ -1,3 +1,21 @@
-let paises = ["Cuba", "Holanda", "China", "Chile"];
-paises[2] = "Japão";
-console.log(paises);
+function validarNota(nota1,nota2){
+    if(nota1<=10 && nota1>=0 && nota2<=10 && nota2>=0){
+        return true;
+    }else{
+        return false
+    }
+}
+
+function contaNota(nota1,nota2){
+    return (nota1 + nota2) / 2
+}
+
+function totalNota(nota1,nota2){
+    if(validarNota(nota1,nota2)){
+        console.log(contaNota(nota1,nota2))
+    }else{
+        console.log("erro")
+    }
+}
+
+totalNota(9,9)
